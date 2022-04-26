92071d8d2634ed60784927266b9b9d8f49de1782
Abinash Adhikari <abiwaseda@gmail.com>
2022-04-26 13:00:14 +0900

    try next-ui instead of mui

--- src/components/layouts/PCLayout.tsx
+++ src/components/layouts/PCLayout.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Box, CssBaseline } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import CommonAppBar from "../common/CommonAppBar";
 import CommonDrawer from "../common/CommonDrawer";
 import CommonHeadTag, { HeadTagMeta } from "./CommonHeadTag";
-
-const theme = createTheme();
 
 interface PCLayoutProps {
   children: React.ReactNode;
@@ -27,23 +24,21 @@ const PCLayout: React.FC<PCLayoutProps> = (props: PCLayoutProps) => {
 
   return (
     <div>
-      <ThemeProvider theme={theme}>
-        <CommonHeadTag meta={meta} />
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <CommonAppBar
-            open={open}
-            drawerWidth={drawerWidth}
-            toggleDrawer={toggleDrawer}
-          />
-          <CommonDrawer
-            open={open}
-            drawerWidth={drawerWidth}
-            toggleDrawer={toggleDrawer}
-          />
-          {children}
-        </Box>
-      </ThemeProvider>
+      <CommonHeadTag meta={meta} />
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <CommonAppBar
+          open={open}
+          drawerWidth={drawerWidth}
+          toggleDrawer={toggleDrawer}
+        />
+        <CommonDrawer
+          open={open}
+          drawerWidth={drawerWidth}
+          toggleDrawer={toggleDrawer}
+        />
+        {children}
+      </Box>
     </div>
   );
 };
